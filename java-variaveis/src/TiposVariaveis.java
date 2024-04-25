d9c249ed99baec5f3bdb26acdbe06b4e500b64e0
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-04-25 14:52:12 -0300

    Feat: Tipos de variaveis

--- java-variaveis/src/TiposVariaveis.java
+++ java-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // caso começe com zero, deve ser de outro dipo como String
+        long cpf = 98765432109L; // caso começe com zero, deve ser de outro dipo como String. L no final sempre
+        float pi = 3.14F; // F no final sempre
+        double salario = 2500.33;
+
     }
 }
