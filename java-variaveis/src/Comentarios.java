1c291c0fc623faccd8bf189b57573a740127cc52
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-04-29 09:38:43 -0300

    Feat: Comentario Documentação java

---
+++ java-variaveis/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        
+        //Comentario de linha unica
+
+        /*
+         * comentario de uma ou mais linhas
+         */
+
+    }
+    /**
+     * comentário a nivel de documentação com um * a mais
+     */
+    public void metodo(){
+         }
+}
