c7da4ca177e000bbef9daf96c19c571aea3d95db
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-04-29 13:44:56 -0300

    Feat:  Maneira de executar deixando o código mais limpo

--- controleDeFluxo/src/ResultadoEscolar.java
+++ controleDeFluxo/src/ResultadoEscolar.java
@@ -1,11 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
-       
-       if(nota >= 7)
-        System.out.println("Aprovado");
+        int nota = 10;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
 
-       else
-        System.out.println("Reprovado");
     }
 }
