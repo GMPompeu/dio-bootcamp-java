19c87388aeccf2b6b5ff22dce0da018efa96c8d6
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-04-29 13:45:53 -0300

    Condição ternaria

--- controleDeFluxo/src/ResultadoEscolar.java
+++ controleDeFluxo/src/ResultadoEscolar.java
@@ -1,6 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 10;
+        int nota = 6;
 		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
 		System.out.println(resultado);
 
