9dcc576ca054f7f44e0e756d43ce1f062ee4f4d9
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-05-20 09:52:59 -0300

    Emcapsulamento

--- java-poo/src/ServicoPai.java
+++ java-poo/src/ServicoPai.java
@@ -1,17 +1,4 @@
-public class ServicoPai {
-    public void enviarMensagem() {
-		validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-		salvarHistoricoMensagem();
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-	}
-	
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
-}
+public abstract class ServicoPai {
+    public abstract void enviarMensagem();
+    public abstract void receberMensagem();
+}   
