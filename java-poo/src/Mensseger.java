d928519920b36a3e90b9b598976cf71c1be4ed56
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-05-20 09:54:45 -0300

    Abstrac servico pai

--- java-poo/src/Mensseger.java
+++ java-poo/src/Mensseger.java
@@ -1,3 +1,9 @@
 public class Mensseger extends ServicoPai{
-    
+
+    public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo Telegram");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Telegram");
+	}
 }
